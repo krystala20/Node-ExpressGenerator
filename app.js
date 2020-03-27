e0e0004e3674afbdcc17e76bff38ee3b545a6553
KrysA19 <krystalokehie@gmail.com>
2020-03-26 21:35:22 -0700

    Express Session Part 2

--- app.js
+++ app.js
@@ -51,33 +51,20 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 //Add Authentication: so users authenticate before accessing data from server
 function auth(req, res, next) {
   console.log(req.session);
 
  if (!req.session.user) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
         const err  = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
         err.status = 401;
         return next(err);
-      }
-
-      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':')
-      const user = auth[0];
-      const pass = auth[1];
-      if (user === 'admin' && pass == 'password') {
-        req.session.user = 'admin';
-        return next(); //authorized
-      } else {
-        const err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status =401;
-        return next(err);
-      }
- } else {
-    if(req.session.user === 'admin') {
+      
+  } else {
+    if(req.session.user === 'authenticated') {
         //grant access by passing client to next middleware function using next
         return next();
     } else {
@@ -86,16 +73,13 @@ function auth(req, res, next) {
         return next(err);
     }
  }
-
 }
 
 app.use(auth);
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
