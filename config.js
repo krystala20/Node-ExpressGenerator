39ba7e96943ff27407f372007406052d4e44c993
KrysA19 <krystalokehie@gmail.com>
2020-03-31 13:35:58 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
     //will use this to sign JWT later
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '1413544462165330',
+        clientSecret: '13abbb881cd4c600348640b34646dbfa'
+    }
 }
